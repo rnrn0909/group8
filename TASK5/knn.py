bd4634765fddee0efba36d4bb48769e994a9fae1
rnrn0909 <kimchaer@b-tu.de>
2022-01-29 01:28:33 +0100

    feature importance score

--- TASK5/knn.py
+++ TASK5/knn.py
@@ -5,14 +5,13 @@ from sklearn.model_selection import train_test_split, cross_val_score, RepeatedK
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
-from sklearn import metrics
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn.preprocessing import RobustScaler
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MaxAbsScaler
 from sklearn.preprocessing import PowerTransformer
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import classification_report
-# from sklearn.linear_model import RidgeCV, LassoCV, Ridge, Lasso
 import sys
 import os
 import json
@@ -28,15 +27,6 @@ def featurelist():
                 filelist.append(file)
     return filelist
 
-def even_odd(lst):      # Since there is no specific label in my dataset,
-    if len(lst) % 2 == 0:       # case: even
-        data_target = [0]*(len(lst)//2) +[1]*(len(lst)//2)            # dummy label, nr of each label = len(datalist)//2
-        return data_target
-    elif len(lst) % 2 == 1:     # case: odd
-        data_target = [0]*(len(lst)//2) + [1]*(len(lst)//2 + 1)
-        return data_target
-    else:
-        pass
 
 def geturlhash():
     with open("./urlhashes.json") as data_file:
@@ -49,28 +39,32 @@ def hundreddomain():
                                       host='127.0.0.1',
                                       database='webprintdb', auth_plugin='mysql_native_password')
 
-    query = """SELECT url FROM websites"""
+    query = """SELECT id_mainpage, url FROM websites"""
     cursor = conn.cursor()
     cursor.execute(query)
-    for url in cursor:
-        validurl.append(urlparse(url[0]).netloc)
+    for id_mainpage, url in cursor:
+        validurl.append(id_mainpage)  # use id_mainpage as label/index of array
+        validurl.append(urlparse(url).netloc)
     conn.close()
+    validurl = np.array(validurl)
+    validurl = np.reshape(validurl, (len(validurl) // 2, 2))
     return validurl
 
 def create_label():
-    target_label=[]
+    target_label = []
     validurl = hundreddomain()
     filelist = featurelist()
-    for x in range(len(filelist)):
-        data = geturlhash()
-        filehash = filelist[x].split('_')[2]
-        for url, savedhash in data.items():
+    urldata = geturlhash()
+    for file in filelist:
+        filehash = file.split('_')[2]
+        for url, savedhash in urldata.items():
             if filehash == savedhash:
                 domain = urlparse(url).netloc
-                if domain in validurl:
-                    target_label.append(1)
-                else:
-                    target_label.append(0)
+                for y in range(len(validurl)):
+                    if domain == validurl[y][1]:
+                        target_label.append(validurl[y][0])
+                    else:
+                        pass
     return target_label
 
 def main():
@@ -86,11 +80,10 @@ def main():
                 'SIZE_IN_MEAN', 'SIZE_IN_MIN', 'SIZE_IN_MAX', 'SIZE_IN_STD', 'SIZE_OUT_MEAN', 'SIZE_OUT_MIN',
                 'SIZE_OUT_MAX', 'SIZE_OUT_STD',
                 'SIZE_IN_TOTAL', 'SIZE_OUT_TOTAL', 'TCP_MEAN', 'TCP_STD', 'TCP_MAX', 'TLS_MEAN', 'TLS_STD', 'TLS_MAX']
+    n_feats = len(col_list)
     # number of packets in type of trace affects 10% (the most powerful feature)
     # and traces from same url have all same value for 'TCP_MEAN', 'TCP_STD', 'TCP_MAX', 'TLS_MEAN', 'TLS_STD', 'TLS_MAX'
-    # element_col = []
-    # for z in range(0, 45):
-    #     element_col.append(col_list[z])
+
     filelist = featurelist()
     datalist = []
     print(' Start to read csv files... ')
@@ -99,6 +92,7 @@ def main():
         filepath = './includeOLfeature/' + file
         data = read_csv(filepath)
         data.fillna(0, inplace=True)        # fill NaN value with 0
+        # n_feats = data.shape[1]
         dataset = np.array(data)
         for item in dataset:
             element = np.array(item)        # len(element) = 63
@@ -106,13 +100,6 @@ def main():
             onlyfloatarr = []
             for i in range(len(element)):          # len(element) = 52
                 onlyfloatarr.append(float(element[i]))
-
-            # for j in range(56, 62):
-            #     if element[j] != 'None':
-            #         onlyfloatarr.append(float(element[j]))
-            #     else:
-            #         element[j] = 0
-            #         onlyfloatarr.append(element[j])
             datalist.append(onlyfloatarr)
     data_target = create_label()
 
@@ -144,20 +131,8 @@ def main():
         x_test = scaler.fit_transform(x_test)
     else:
         print('Invalid option. No scaling. ')
-        sys.exit()
+        sys.exit()              # stop running
 
-    # j_range = range(1, 100)     # to find best number of neighbors
-    # k_score = []
-    # for j in j_range:
-    #     knn = KNeighborsClassifier(j)       # nr of neighbor = j
-    #     scores = cross_val_score(knn, x_train, y_train, cv=40, scoring="accuracy")  # 10-fold cross-validation
-    #     k_score.append(scores.mean())
-    # plt.plot(j_range, k_score)
-    # plt.xlabel('Value of K for KNN')
-    # plt.ylabel('Cross-Validation Accuracy')
-    # plt.show()              # based on the result, best: j = 1
-    # thus show the plot first, then make user to select k
-    # nrofNeighbor = input('number of neighbor? ')
     print("")
     print('Please choose the number of cross validation. The number must be bigger than 1. ')
     nrtofold = input('k? ')
@@ -165,24 +140,26 @@ def main():
     clf.fit(x_train, y_train)
     kf = RepeatedKFold(n_splits=int(nrtofold), n_repeats=40, random_state=1)
     score = cross_val_score(clf, x_train, y_train, cv=kf, scoring="accuracy")
-    print('Score:', '%.3f' % score.mean())
+    print('CV Score: %.3f' % score.mean())
     y_pred = clf.predict(x_test)
-    # print('Accuracy :', metrics.accuracy_score(y_test, y_pred))
+    print('Accuracy : %.3f' % accuracy_score(y_test, y_pred))
     print(classification_report(y_test, y_pred))            # including accuracy score, recall...etc.
-    # knnclassifier doesn't have feature_importance_
-    # how to show?
+    feature_i_score = []
+    print('Feature\t\t\tAccuracy: ')
+    for n in range(n_feats):
+        datalist = np.array(datalist)
+        new_x = datalist[:, n].reshape(-1, 1)
+        scores = cross_val_score(clf, new_x, data_target, cv=kf, scoring="accuracy")
+        print(f'{col_list[n]}', '\t\t\t', '%.6f' % scores.mean())
+        feature_i_score.append(scores.mean())
 
-    #  then the best way to estimate feature importance is by taking the sample to predict on,
-    #  and computing its distance from each of its nearest neighbors for each feature (call these neighb_dist).
-    #  Then do the same computations for a few random points (call these rand_dist) instead of the nearest neighbors.
-    #  Then for each feature, you take the ratio of neighb_dist / rand_dist, and the smaller the ratio, the more important that feature is.
-    #  KNN is suited for lower dimensional data.
-    #  KNN can benefit from feature selection that reduces the dimensionality of the input feature space.
+    print(feature_i_score.sort(reverse=True))
+    # how to show from the most important feature with feature name?
+
+
 
 if __name__ == "__main__":
-
     main()
-
 
 
 # https://python-course.eu/machine-learning/k-nearest-neighbor-classifier-in-python.php
@@ -191,7 +168,12 @@ if __name__ == "__main__":
 # https://machinelearningmastery.com/k-nearest-neighbors-for-machine-learning/
 # https://github.com/WillKoehrsen/feature-selector
 # Random KNN feature selection - a fast and stable alternative to Random Forests, Shengqiao Li et al.
-
+#  then the best way to estimate feature importance is by taking the sample to predict on,
+#  and computing its distance from each of its nearest neighbors for each feature (call these neighb_dist).
+#  Then do the same computations for a few random points (call these rand_dist) instead of the nearest neighbors.
+#  Then for each feature, you take the ratio of neighb_dist / rand_dist, and the smaller the ratio, the more important that feature is.
+#  KNN is suited for lower dimensional data.
+#  KNN can benefit from feature selection that reduces the dimensionality of the input feature space.
 
 
 # def str_column_to_float(dataset, column):
